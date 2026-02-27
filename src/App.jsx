c93f63ed90d8901129e11811f179ac099543715a
cute jaat <meenukataria2207@gmial.com>
2026-02-27 19:07:00 +0530

    day4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import New from "./Components/new";
+
+    
+    import { useEffect, useState } from "react"
 
 export default function App(){
+
+  const[count, setCount] = useState(0);
+  const[start,setStart] = useState(false);
+
+  const handle = ()=>{
+    setStart(true);
+  }
+  useEffect(()=>{
+
+    if(!start) return;
+    const timer = setInterval(()=>{
+      setCount(count=> count+1);
+    },1000)
+
+    return () => clearInterval(timer);
+  },[start]);
+
   return(
     <>
-    
-    <h1> Learning React</h1>
-    <New  name="Meenu" age={18} address="Haryana"/>
-    <New  name="Jaat" age={18} address="Sonipat"/>
+    <h1>Count {count}</h1>
+    <button onClick={handle}>Start</button>
     </>
   )
 }
+  
